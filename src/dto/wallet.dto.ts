c032b5c9af8a447478965d0d3e47e6df57167ad8
AY4LIFE <168647944+AY4LIFE@users.noreply.github.com>
2026-06-15 09:24:23 -0600

    Add createWalletDto for wallet creation with currency validation

---
+++ src/dto/wallet.dto.ts
@@ -0,0 +1,14 @@
+import {IsString, IsNotEmpty, IsIn} from "class-validator"
+
+// This DTO is used when someone is creating a wallet
+// We only need one field - the currenct they want
+
+export class createWalletDto{
+    // @IsIn checks that the value is in one of the allowed options
+    @IsString()
+    @IsNotEmpty()
+    @IsIn(["NGN","ngn", "GHS", "ghs", "USD", "usd"], {
+        message: "Currency must be one of: NGN, USD, GHS"
+    })
+    currency: string = ""
+}
