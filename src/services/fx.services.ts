0b13b7831c673f551ee602bbb40d90251a8d0311
AY4LIFE <168647944+AY4LIFE@users.noreply.github.com>
2026-06-18 04:53:54 -0600

    Enhance FxService to handle the full Cahe-Aside Pattern by fetching exchange rates from API and storing them in Redis

--- src/services/fx.services.ts
+++ src/services/fx.services.ts
@@ -27,7 +27,62 @@ export class FxService {
         return parseFloat(cached)
     } // Cache Hit - Return immediately, no API calls needed
 
-    
+    // ------------------------------------
+    // STEP 2 — CACHE MISS → FETCH FROM API
+    // Only reaches here if Redis didn't have the rate.
+    // Call the free exchangerate API with the base currency
+    console.log(`Cache miss for ${cacheKey} - fetching from API`)
+    const response = await fetch(
+        `https://api.exchangerate-api.com/v4/latest/${from}`
+    )
+    if(!response.ok){
+        throw new Error(`Failed to fetch exchange rate for ${from}`)
+    }
+    const data = await response.json() as {rates: Record<string, number>}
+
+    // Extract the specific rate we need
+    const rate = data.rates[to]
+    if (!rate){
+        throw new Error (`Exchange rate not found for ${from} -> ${to}`)
+    }
+
+    // STEP 3: Store in Redis with a TIL (EX)
+    await redis.set(cacheKey, rate.toString(), {EX: 300})
+    return rate
 
     }
+
+    // ------------------------------------
+  // CONVERT
+  // Takes an amount in one currency and converts it to another.
+  // Reuses getRate() so caching applies here too automatically.
+  //
+  // e.g. convert('NGN', 'USD', 5000) → 3.25
+  // ------------------------------------
+
+  async convert(
+    from: string,
+    to: string,
+    amount: number
+  ): Promise<{
+    from: string,
+    to: string,
+    amount: number,
+    rate: number,
+    result: number
+    
+  }>{
+    const rate = await this.getRate(from, to)
+    // ------------------------------------
+    // PRECISION
+    // We round to 4 decimal places — same precision
+    // as our wallet balance columns in the DB
+    // ------------------------------------
+
+    const result = parseFloat((amount * rate).toFixed(4))
+
+    return {
+        from, to, amount, rate, result
+    }
+  }
 }
