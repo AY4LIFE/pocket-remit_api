de2f108b5f508a2a96207ebe2e88b3aa0a9ac83c
AY4LIFE <168647944+AY4LIFE@users.noreply.github.com>
2026-06-22 14:54:01 +0100

    Add security middleware: implement helmet and rate limiting for global and auth routes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import type { ErrorRequestHandler, NextFunction, Request, Response } from "expre
 import dotenv from "dotenv";
 import devLogger from './utils/logger.js'
 import authRoutes from "./routes/auth.routes.js"
+import helmet from "helmet"
+import {rateLimit} from 'express-rate-limit'
 import "reflect-metadata";
 import { AppDataSource } from "./config/database.js"
 import walletRoutes from "./routes/wallet.routes.js"
@@ -32,10 +34,56 @@ dotenv.config(); // loads your .env file
 
 const app = express();
 
+// Helmet must be the first middleware registered
+// It sets 13 security HTTP headers automatically
+app.use(helmet())
+
+// Global Rate Limiter
+// Applies to all routes - a basic safety net
+// 100 requests per 15 minutes per IP address
+// Protects against general API abuse and DDoS
+
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  message: {
+    success: false,
+    message: 'Too many requests, please try again later'
+  },
+  standardHeaders: true,  // sends RateLimit headers in response
+  legacyHeaders: false    // disables old X-RateLimit headers
+})
+app.use(globalLimiter)
+
+// ------------------------------------
+// AUTH RATE LIMITER
+// Much stricter — applied ONLY to /auth routes.
+// Why stricter? Because /auth/login is the main
+// target for brute force attacks.
+//
+// 5 attempts per 15 minutes per IP.
+// After 5 failed logins, the attacker is blocked.
+// ------------------------------------
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: {
+    success: false,
+    message: 'Too many login attempts, please try again in 15 minutes time'
+  },
+  standardHeaders: true,
+  legacyHeaders: true,
+  // SKIP SUCCESSFUL REQUESTS
+  // Only COUNT failed attempts toward the limit.
+  // A real user logging in successfully doesn't
+  // get penalised — only failed attempts stack up.
+  skipSuccessfulRequests: true
+})
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/auth", authRoutes)
+app.use("/auth", authLimiter, authRoutes) // authLimiter added
 app.use("/wallets", walletRoutes)
 app.use("/transfers", transferRoutes)
 app.use('/fx', fxRoutes)
