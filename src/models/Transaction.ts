383fe024b8a9cbd8dcbf80ffc46fd05df9585b9d
Ayomide Akinsanya <168647944+AY4LIFE@users.noreply.github.com>
2026-07-10 06:30:57 +0000

    Fix narration property type in Transaction model to be optional

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -57,7 +57,7 @@ export class Transaction{
   bankCode!: string
 
   @Column({nullable: true})
-  narration!: true
+  narration?: string
 
   // ------------------------------------
   // PROVIDER FIELDS
