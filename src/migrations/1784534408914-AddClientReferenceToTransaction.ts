1f2900a68f7701f237680d1d99ddb6f1796bfe48
Ayomide Akinsanya <168647944+AY4LIFE@users.noreply.github.com>
2026-07-20 08:00:46 +0000

    Add migration to include clientReference field in Transaction table

---
+++ src/migrations/1784534408914-AddClientReferenceToTransaction.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddClientReferenceToTransaction1784534408914 implements MigrationInterface {
+    name = 'AddClientReferenceToTransaction1784534408914'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "transaction" ADD "clientReference" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "transaction" ADD CONSTRAINT "UQ_3b35593cae1d7f3c40860659844" UNIQUE ("clientReference")`);
+        await queryRunner.query(`ALTER TABLE "transaction" DROP COLUMN "narration"`);
+        await queryRunner.query(`ALTER TABLE "transaction" ADD "narration" character varying`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "transaction" DROP COLUMN "narration"`);
+        await queryRunner.query(`ALTER TABLE "transaction" ADD "narration" boolean`);
+        await queryRunner.query(`ALTER TABLE "transaction" DROP CONSTRAINT "UQ_3b35593cae1d7f3c40860659844"`);
+        await queryRunner.query(`ALTER TABLE "transaction" DROP COLUMN "clientReference"`);
+    }
+
+}
